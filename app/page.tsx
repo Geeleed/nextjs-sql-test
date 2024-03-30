7ee82a6af4315978d064008eaca614feec7e79be
Geeleed <surasak.kaewpho@gmail.com>
2024-03-30 14:23:24 +0700

    เชื่อมต่อ สร้าง table และเพิ่มข้อมูลเบื้องต้นได้แล้ว

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
+// import { sql } from "@vercel/postgres";
+
+// export default async function Cart({
+//   params,
+// }: {
+//   params: { user: string };
+// }): Promise<JSX.Element> {
+//   const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+//   return (
+//     <div>
+//       {rows.map((row) => (
+//         <div key={row.id}>
+//           {row.id} - {row.quantity}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 import React from "react";
 
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <div>
+      ทดสอบเชื่อมต่อและใช้งาน Postgresql ของ vercel ก็เชื่อมต่อ db ได้ปกติ
+    </div>
+  );
 }
